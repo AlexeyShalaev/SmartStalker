84d94156bb8cc9b29d243292a96ba2b10ce2a135
AlexeyShalaev <75322386+AlexeyShalaev@users.noreply.github.com>
2022-04-21 17:05:00 +0300

    potected

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 settings = {
     # Database
-    'host': '45.90.216.251',
-    'user': 'lsuser_predrof',
-    'database': 'lsuser_predprof',
-    'password': '!predrofPass',
+    'host': '127.0.0.0',
+    'user': 'user',
+    'database': 'database',
+    'password': 'password',
     # App Settings
-    'secret_key': '6ad1107743e3546cd7d3328375ef0b5771d5a9bd',
+    'secret_key': 'secret_key',
     'MAX_CONTENT_LENGTH': 4096*4096
 }
